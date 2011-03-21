7fa0fb0f63c6b3e2b966f3e88c1c72ae693d6d37
Pablo Alejandro Costesich <rhfixer@gmail.com>
2011-03-21 01:57:59 -0300

    Added basic buffering to strings (parser req.)

---
+++ cnetstring.c
@@ -0,0 +1,89 @@
+/*
+ * cnetstring.c
+ *
+ * Copyright 2011, Pablo Alejandro Costesich
+ *
+ * Licensed under Three-Clause BSD
+ *
+ */
+
+#include <stdlib.h>
+
+struct CTNetStr{
+    tns_type   type;
+    void *      payload;
+};
+
+static typedef struct{
+    unsigned size;
+    unsigned used;
+    char * str;
+} strbuffer;
+
+
+static int _read(char **, FILE *);
+static char * _pop_buffer(strbuffer *);
+static strbuffer * _push_buffer(strbuffer *, char);
+static strbuffer * _init_buffer(strbuffer *);
+
+tnetstr * tns_parse(char *, FILE *){
+
+}
+
+static char * _pop_buffer(strbuffer * buf){
+    char * s = realloc(buf->str, buf->used + 1);
+    s[buf->used + 1] = 0;
+    if (_init_buffer(buf) == NULL){
+         buf->str = s;
+         buf->size = buf->used + 1;
+         return NULL;
+    }
+    return s;
+}
+
+static strbuffer * _init_buffer(strbuffer * buf){
+    char * s = malloc(TNS_BUFF_SIZE);
+
+    if (s == NULL)
+        return NULL;
+
+    buf->size = TNS_BUFF_SIZE;
+    buf->used = 0;
+    buf->str = s;
+
+    return buf;
+}
+
+static strbuffer * _push_buffer(strbuffer * buf, char c){
+    char * aux;
+
+    if (buf->size < buf->used + 1){
+        aux = realloc(buf->str, buf->size * 2);
+        if (aux == NULL)
+            return NULL;
+        buf->str = aux;
+    }
+
+    buf->str[buf->used++] = c;
+    return buf;
+}
+
+static int _read(char **s, FILE *f){
+    int c;
+
+    if (s != NULL){
+        c = **s;
+        *s++;
+    } else if (f != NULL){
+        c = fgetc(f);
+    } else {
+        c = EOF;
+    }
+
+    return c;
+}
+
+
+tns_type tns_get_type(tnetstr * tns){
+    return tns->type;
+}
---
+++ cnetstring.h
@@ -0,0 +1,44 @@
+/*
+ * cnetstring.h
+ *
+ * Copyright 2011, Pablo Alejandro Costesich
+ *
+ * Licensed under Three-Clause BSD
+ *
+ */
+
+#ifndef _CNETSTR
+#define _CNETSTR 0.1
+#include "ll.h"
+#include "ht.h"
+#include <stdio.h>
+
+#define TNS_BUFF_SIZE 16
+
+typedef struct CTNetStr tnetstr;
+typedef enum CTNS_Types {
+    tns_String,
+    tns_List,
+    tns_HT,
+    tns_Number,
+    tns_None,
+    tns_Boolean
+} tns_type;
+
+const char tns_type_repr[] = {
+    "\"",
+    "]",
+    "}",
+    "#",
+    "~",
+    "!"
+};
+
+#define tns_parse_stream(F) tns_parse(NULL, (F))
+#define tns_parse_string(S) tns_parse((S), NULL)
+
+tnetstr *   tns_parse(char *, FILE *);
+tnetstr *   tns_parse_string(char *);
+tnetstr *   tns_parse_stream(FILE *);
+tns_type   tns_get_type(tnetstr *);
+#endif
